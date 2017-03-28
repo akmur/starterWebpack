2db4555598bdc4da976630abdc2880c5443c30a4
Alex Muraro <akmuraro@gmail.com>
2017-03-28 11:43:53 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
 const path = require('path');
+const unminifiedWebpackPlugin = require('unminified-webpack-plugin');
 
 module.exports = {
-    entry: {
-        filename: './src/js/main.js'
-    },
+    entry: './src/js/main.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/main.bundle.js'
     },
     module: {
-        loaders: [
+        rules: [
             {
-            test: /\.js$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',
-            query: {
-                    presets: ['es2015']
+                test: /\.js$/,
+                include: [
+                    path.resolve(__dirname, 'src/js')
+                ],
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                options: {
+                    presets: ["es2015"]
                 }
             }
         ]
-    }
+    },
+    devtool: 'nosources-source-map',
+    plugins: [
+        new unminifiedWebpackPlugin()
+    ]
 };

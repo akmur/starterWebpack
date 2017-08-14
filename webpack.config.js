e9b74aae42404a76d63cb4c40350380d06f9c460
Alessandro Muraro <a.muraro@navabi.de>
2017-08-14 12:36:42 +0200

    updated packages, webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,26 @@
+/* global module require __dirname */
+
 const path = require('path');
-const UnminifiedWebpackPlugin = require('unminified-webpack-plugin');
-
-let devToolType;
-
-if (process.env.environment === 'dist') {
-  devToolType = 'source-map';
-} else {
-  devToolType = 'eval-source-map';
-}
 
 module.exports = {
   entry: './src/js/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'js/main.bundle.js',
+    filename: 'js/main.bundle.js'
   },
   module: {
-    rules: [
+    loaders: [
       {
         test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src/js'),
-        ],
-        exclude: /(node_modules|bower_components)/,
         loader: 'babel-loader',
-        options: {
-          presets: ['es2015'],
-        },
-      },
-    ],
+        query: {
+          presets: ['env']
+        }
+      }
+    ]
   },
-  devtool: devToolType,
-  plugins: [
-    new UnminifiedWebpackPlugin(),
-  ],
+  stats: {
+      colors: true
+  },
+  devtool: 'source-map'
 };

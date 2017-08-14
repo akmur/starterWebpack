f633f64bcab974d0acea01f809fb4ce799acbfdf
Alessandro Muraro <a.muraro@navabi.de>
2017-08-14 12:38:34 +0200

    added examples in main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 /* global document sweetAlert */
 
-// import $ from 'jquery';
+import $ from 'jquery';
 import app from './app';
-// import './sweet-alert';
+import './sweet-alert';
 
 document.addEventListener('DOMContentLoaded', () => {
   app.init(); // testing my custom module
-  // console.log($); // testing jQuery
-  // sweetAlert('Hello world!'); // testing old skool external plugin import
+  console.log($); // testing jQuery
+  sweetAlert('Hello world!'); // testing old skool external plugin import
 });
